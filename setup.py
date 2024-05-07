a7291dbfbe2979562442253c582f9639ea71bcd9
Tej313 <153349011+Tej313@users.noreply.github.com>
2024-05-07 13:04:40 +0545

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="helloworld",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # Dependencies list, e.g., ['numpy>=1.18.1']
+    author="Tej Bhatta",
+    author_email="tejbro02@gmail.com",
+    description="A simple description of my project.",
+    license="MIT",
+    keywords="helloworld project",
+    url="http://www.facebook.com/tejbro11",  # Project URL
+)
